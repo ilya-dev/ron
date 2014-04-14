45c9733ab4eba25ad14ef65f9c660d52e23cfd76
Ilya S <public.ilya.s@gmail.com>
2014-04-14 14:13:34 +0700

    Add Ron::setReflector and Ron::getReflector

--- src/Ron/Ron.php
+++ src/Ron/Ron.php
@@ -2,7 +2,44 @@
 
 class Ron {
 
+    /**
+     * The reflector you work with
+     *
+     * @var \ReflectionClass
+     */
+    protected $reflector;
 
+    /**
+     * The constructor
+     *
+     * @param \ReflectionClass $reflector
+     * @return \Ron\Ron
+     */
+    public function __construct(\ReflectionClass $reflector)
+    {
+        $this->setReflector($reflector);
+    }
+
+    /**
+     * Sets the reflector
+     *
+     * @param \ReflectionClass $reflector
+     * @return void
+     */
+    public function setReflector(\ReflectionClass $reflector)
+    {
+        $this->reflector = $reflector;
+    }
+
+    /**
+     * Returns the reflector
+     *
+     * @return \ReflectionClass
+     */
+    public function getReflector()
+    {
+        return $this->reflector;
+    }
 
 }
 
--- tests/Spec/Ron/RonSpec.php
+++ tests/Spec/Ron/RonSpec.php
@@ -2,12 +2,25 @@
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use ReflectionClass as Reflector;
 
 class RonSpec extends ObjectBehavior {
+
+    function let(Reflector $reflector)
+    {
+        $this->beConstructedWith($reflector);
+    }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('Ron\Ron');
+    }
+
+    function it_sets_and_returns_the_reflector(Reflector $reflector)
+    {
+        $this->setReflector($reflector);
+
+        $this->getReflector()->shouldReturn($reflector);
     }
 
 }
